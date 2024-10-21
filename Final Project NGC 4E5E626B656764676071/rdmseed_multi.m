a12f86c99f40c15401856081aa66e60f8018e565
upmanyu9101 <73416399+upmanyu9101@users.noreply.github.com>
2024-10-21 21:48:47 +0530

    Update rdmseed_multi.m

--- Final Project NGC 4E5E626B656764676071/rdmseed_multi.m
+++ Final Project NGC 4E5E626B656764676071/rdmseed_multi.m
@@ -1,1 +1,9 @@
 Sorry for the inconvenience but we reorganized the repository, you can go through the full repository by using the following link:
+
+https://github.com/upmanyu9101/Martian-and-Lunar-Seismic-activity/tree/main
+
+Or if you would like to just access the matlab code you can go to this link
+https://github.com/upmanyu9101/Martian-and-Lunar-Seismic-activity/blob/main/Final%20Project%20NGC%204E5E626B656764676071/ngc_4e5e.m
+
+We also wrote Assessment of the project, which can be acceses from here:
+https://github.com/upmanyu9101/Martian-and-Lunar-Seismic-activity/blob/main/Assessment%20of%20the%20program%20-%20ngc_4e5e.pdf
